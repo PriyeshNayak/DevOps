9d0843eeb8b595087fb35f9691879c7a1527ed01
PriyeshNayak <92969539+PriyeshNayak@users.noreply.github.com>
2023-12-12 13:30:26 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 # config-repo/env.sh
 
-echo 'API Key: "${{env.API_KEY}}"' >> .env
-echo 'API Key: "${{env.API_KEY}}"'
-echo 'Database URL: "${{env.DATABASE_URL}}"' >> .env
+echo 'API_Key: "${{env.API_KEY}}"' >> .env
+echo 'API_Key: "${{env.API_KEY}}"'
+echo 'Database_URL: "${{env.DATABASE_URL}}"' >> .env
 ls -la
 cat .env
