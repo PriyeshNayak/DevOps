cb4e3977e50ee170d9d8ed292fe9007451333b04
PriyeshNayak <92969539+PriyeshNayak@users.noreply.github.com>
2023-12-13 12:24:45 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source .env
-echo API_Key= ${{secrets.API_KEY}} >> .env
-echo Database_URL=${{secrets.DATABASE_URL}} >> .env
+echo API_Key= ${{secrets.API_KEY}} 
+echo Database_URL=${{secrets.DATABASE_URL}} 
